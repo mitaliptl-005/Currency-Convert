65c5d152e64edd7a3c0330dc2ca9d1d2941466ee
Mitali Patel <mitali572003@gmail.com>
2025-06-26 07:39:00 +0530

    Initial commit

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,58 @@
+import React, { useId } from 'react';
+
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) {
+  const amountInputId = useId();
+
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label htmlFor={amountInputId} className="inline-block mb-2 text-black/40">
+          {label}
+        </label>
+        <input
+          id={amountInputId}
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+        />
+      </div>
+
+      <div className="flex flex-wrap justify-end w-1/2 text-right">
+        <label className="w-full mb-2 text-black/40">Currency Type</label>
+        <select
+          className="px-2 py-1 bg-gray-100 rounded-lg outline-none cursor-pointer"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        >
+          {/* Default placeholder option */}
+          {selectCurrency === "" && (
+            <option value="" disabled>
+              Select currency
+            </option>
+          )}
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency.toUpperCase()}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
